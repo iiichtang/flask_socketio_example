b06a103cee4163e5bb0229beda519fd755f01063
iiichtang <iiichtang@gmail.com>
2015-11-17 15:44:41 +0800

    add docstring

--- 06_flask_authendication.py
+++ 06_flask_authendication.py
@@ -16,7 +16,9 @@ def check_auth(username, password):
 
 
 def authenticate():
-    """Sends a 401 response that enables basic auth"""
+    """
+    Sends a 401 response that enables basic auth
+    """
     return Response('Could not verify your access level for that URL.\n'
                     'You have to login with proper credentials', 401,
                     {'WWW-Authenticate': 'Basic realm="Login Required"'})
